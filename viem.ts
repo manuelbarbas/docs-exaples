b1f9757597c63c407e976fd874808e28a8d702c5
manuelbarbas <manuel@skalelabs.com>
2024-04-17 16:11:37 +0100

    recipe viem example

---
+++ viem.ts
@@ -0,0 +1,37 @@
+import { createWalletClient, custom,http ,createPublicClient} from 'viem'
+import { privateKeyToAccount } from 'viem/accounts'
+import { skaleNebulaTestnet } from "viem/chains";
+import {abi} from "./contract";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const publicClient = createPublicClient({
+    chain: skaleNebulaTestnet,
+    transport: http()
+  })
+
+const pk = process.env.PRIVATE_KEY || "";
+const account = privateKeyToAccount(`0x${pk}`);
+
+const contract_address = "0x4487AF7f18044A99927e81CC628F558F3D091419";
+
+const walletClient = createWalletClient({
+    chain: skaleNebulaTestnet,
+    transport: http(),
+  })
+
+
+// Send transaction to smart contract to update message
+async function ContractSendTx() {    
+    
+    const { request } = await publicClient.simulateContract({
+        address: contract_address,
+        abi: abi,
+        functionName: 'mintTest',
+        args: ["0x_some wallet address"],
+        account
+      })
+    const tx =  await walletClient.writeContract(request)
+    console.log(tx);
+}
+ContractSendTx();
